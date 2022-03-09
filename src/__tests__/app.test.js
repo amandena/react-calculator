984a01bcf0b3c36a2528da766fa9c9a469860ba8
amandena <manda.m.johnson@gmail.com>
2022-03-08 21:53:00 -0500

    created snapshot test with renderer

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,4 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react'
+import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom/extend-expect'
 import App from '../App'
 
@@ -12,4 +13,9 @@ test('should render App component', () => {
   expect(appElement).toBeInTheDocument()
   expect(appElement).toHaveTextContent('DEL')
   expect(appElement).toContainHTML('</button>')
+})
+
+test('matches snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
 })
