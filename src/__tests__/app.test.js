b39be6fe2c0c60fc3c94c6e33893c7d4bf242a78
amandena <manda.m.johnson@gmail.com>
2022-03-09 23:03:18 -0500

    removed react-testing-library imports and all tests using them

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,19 +1,6 @@
-import { render, screen, cleanup } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom/extend-expect'
 import App from '../App'
-
-afterEach(() => {
-  cleanup()
-})
-
-test('should render App component', () => {
-  render(<App/>)
-  const appElement = screen.getByTestId('app-1')
-  expect(appElement).toBeInTheDocument()
-  expect(appElement).toHaveTextContent('DEL')
-  expect(appElement).toContainHTML('</button>')
-})
 
 test('matches snapshot', () => {
   const tree = renderer.create(<App/>).toJSON()
